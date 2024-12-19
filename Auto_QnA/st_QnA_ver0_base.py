a224eb9d4c3dac05ad06971c536eb6233056928d
wlswngks123 <75672074+wlswngks123@users.noreply.github.com>
2024-12-19 15:05:01 +0900

    Update st_QnA_ver0_base.py

--- Auto_QnA/st_QnA_ver0_base.py
+++ Auto_QnA/st_QnA_ver0_base.py
@@ -1,7 +1,7 @@
 import streamlit as st  # Streamlit 라이브러리를 불러옴
 from langchain_openai import ChatOpenAI
 
-openai_api_key = st.secrets["OPENAI_API_KEY"]
+api_key = st.secrets["OPENAI_API_KEY"]
 
 # OpenAI 라이브러리에 API 키 설정
 openai.api_key = api_key
